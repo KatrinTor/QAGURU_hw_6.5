36158ce88d23baee6364f7e1cf2e25577ef56399
Katrin Torsunova <katrin.tep@list.ru>
2023-06-23 11:03:40 +0300

    Base url and headless added

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -1,15 +1,14 @@
 import pytest
 from selene import browser
-from selene.support import webdriver
+from selene.support.shared import browser
 from selenium import webdriver
-from selenium.webdriver.chrome import webdriver
 
 
 @pytest.fixture
 def brwsr():
-    # options = webdriver.ChromeOptions()
-    # options.headless = True
-    # browser.config.driver = webdriver.Chrome(chrome_options=options)
+    options = webdriver.ChromeOptions()
+    options.headless = True
+    browser.config.driver = webdriver.Chrome(options)
     browser.config.window_width = 1900
     browser.config.window_height = 1080
-    browser.open('https://demoqa.com/automation-practice-form')
+    browser.config.base_url = 'https://demoqa.com'
